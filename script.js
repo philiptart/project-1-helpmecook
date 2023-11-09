45a16e5e3f4779d1cbbc20001313e9b32968f2a2
duweri <danielleuweri01@gmail.com>
2023-11-09 19:04:35 +0000

    3rd danielle edit

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ var EdamamAPIKey = "df04c0c3e6mshe64ccb6aa49f000p1ae751jsn3e242baa2eed"
 var recipeInput = document.querySelector(".recipe-input");
 var searchButton = document.querySelector(".search");
 
+const url = 'https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2?type=public&q=chicken';
+const options = {
+	method: 'GET',
+	headers: {
+		'Accept-Language': 'en',
+		'X-RapidAPI-Key': EdamamAPIKey,
+		'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+	}
+};
+
 var getRecipeData = function () {
     var recipeName = recipeInput.value.trim(); //Gets entered recipe name and trims any whitespace
-    var EdamamURL = "https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2" + EdamamAPIKey; //need to find a way to make API work
-
-    fetch(EdamamAPIKey)
+   
+    fetch(url,options)
     .then(function(response){
         return response.json();
 
